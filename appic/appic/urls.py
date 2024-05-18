529a7675ec314d82857e7c2d9359e7d13efbe71c
mszpulak <mszpulak@valuelogic.one>
2024-05-18 12:59:47 +0200

    swagger

--- appic/appic/urls.py
+++ appic/appic/urls.py
@@ -17,10 +17,27 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
-
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 from api.urls import api_router
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="API",
+        default_version="v1",
+        description="description",
+    ),
+    public=True,
+)
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", include(api_router.urls)),
+    path(
+        "swagger/",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    path("redoc/", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"),
 ]
